de2b07810ebd3be8a570f8018bdcd978ac035e82
Akarsha <hegdeakarsha@gmail.com>
2024-05-06 03:26:55 -0400

    merged_data_fetch_6

--- src/Fetchdata/handler.py
+++ src/Fetchdata/handler.py
@@ -20,31 +20,47 @@ def handler(event, context):
     # date1 = current_date.strftime("%Y-%m-%d")
     # date2 = date_7_days_ago.strftime("%Y-%m-%d")
 
-    date1 = "2024-01-01"
-    date2 = "2022-01-01"
+    dates = [
+            "2024-01-01",
+            "2023-01-01",
+            "2022-01-01",
+            "2021-01-01",
+            "2020-01-01",
+            "2019-01-01",
+            "2018-01-01",
+            "2017-01-01"
+    ]
 
-    # API URL
-    api_url = f"https://data.montgomerycountymd.gov/resource/mmzv-x632.csv?$where=crash_date_time between '{date2}T00:00:00' and '{date1}T23:59:59'"
+
+    file_format = f"/tmp/output_api_{dates[0]}_to_{dates[-1]}.csv"
+    with open(file_format, 'w', newline='') as csvfile:
+        for i in range(len(dates)-1):
+            date1 = dates[i]
+            date2 = dates[i+1]
+
+            # API URL
+            api_url = f"https://data.montgomerycountymd.gov/resource/mmzv-x632.csv?$where=crash_date_time between '{date2}T00:00:00' and '{date1}T23:59:59'"
 
 
-    print(f"Invoking api {api_url}")
-    # Send request to API
-    response = requests.get(api_url)
+            print(f"Invoking api {api_url}")
+            # Send request to API
+            response = requests.get(api_url)
 
-    # Check if request was successful
-    if response.status_code == 200:
-        # Write data to CSV file
-        print(response.text)
-        file_format = f"/tmp/output_api_{date1}_to_{date2}.csv"
-        with open(file_format, 'w', newline='') as csvfile:
-            csvfile.write(response.text)
+            # Check if request was successful
+            if response.status_code == 200:
+                # Write data to CSV file
+                print(response.text)
+                
+            
+                csvfile.write(response.text)
+            else:
+                print("Failed to fetch data from API.")
 
         # Upload CSV file to AWS S3 bucket
         s3 = boto3.client('s3')
         bucket_name = 'data-files-infra-myawsbucket-637423608110'
-        folder_path = f'dynamic/{date1}/'
+        folder_path = f'dynamic/{dates[0]}/'
         s3.upload_file(file_format, bucket_name, os.path.join(folder_path, file_format))
 
         print("File uploaded successfully to S3 bucket.")
-    else:
-        print("Failed to fetch data from API.")
+   
--- src/MergeData/handler.py
+++ src/MergeData/handler.py
@@ -1,6 +1,69 @@
 import json
+import csv
+import boto3
+import pandas as pd
+from io import StringIO
+import io
+
 def handler(event, context):
     # Log the event argument for debugging and for use in local development.
     print(json.dumps(event))
+
+    # extract s3 file from event
+
+    record = event['Records'][0]
+    s3_bucket = record['s3']['bucket']['name']
+    s3_key = record['s3']['object']['key']
+
+    print(s3_bucket)
+    print(s3_key)
+
+
+    s3 = boto3.client('s3')
+    bucket_name = s3_bucket
+    api_file_key = s3_key
+
+    # read file saved via api
+    api_object = s3.get_object(Bucket=bucket_name, Key=api_file_key)
+    api_file_data = pd.read_csv(StringIO(api_object['Body'].read().decode('utf-8')))
+
+    print(api_file_data.head())
+
+    # read static file 
+    
+    static_file_key = 'static/static_crash_report.csv'
+
+    static_object = s3.get_object(Bucket=bucket_name, Key=static_file_key)
+    static_file_data = pd.read_csv(StringIO(static_object['Body'].read().decode('utf-8')))
+
+    print(static_file_data.head())
+
+
+#    rename static file column name
+    static_file_data = static_file_data.rename(columns={'Report Number': 'report_number'})
+
+    merged_data=pd.merge(api_file_data, static_file_data, on='report_number', how='inner')
+
+    # print merged_data
+    merged_data.head()
+
+    merged_bucket_name = 'merged-data-infra-637423608110'
+    merged_file_key = 'output/merged.csv'
+
+    with io.StringIO() as csv_buffer:
+        merged_data.to_csv(csv_buffer, index=False)
+
+        response = s3.put_object(
+            Bucket=merged_bucket_name, Key=merged_file_key, Body=csv_buffer.getvalue()
+        )
+
+        status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+
+        if status == 200:
+            print(f"Successful S3 put_object response. Status - {status}")
+        else:
+            print(f"Unsuccessful S3 put_object response. Status - {status}")
+
+
 
     return {}
